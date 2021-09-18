9ce5c31b76324795d933849d3744414df8fc8e4b
Ken Qiu <kenin.qiu@gmail.com>
2021-09-17 21:50:21 -0400

    f

--- src/app/pages/basic/job-add/career-path/career-path.component.ts
+++ src/app/pages/basic/job-add/career-path/career-path.component.ts
@@ -8,7 +8,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { NbSortDirection, NbSortRequest, NbTreeGridDataSource, NbTreeGridDataSourceBuilder } from '@nebular/theme';
 
 interface TreeNode<T> {
-  data: T;
+  value: T;
   children?: TreeNode<T>[];
   expanded?: boolean;
 }
@@ -56,6 +56,20 @@ export class CareerPathComponent implements OnInit{
 
   }
 
+   renameKeys(node, keysMaps) {
+    if(!(node instanceof Object)) return node
+    if(Array.isArray(node)) return node.map(item => this.renameKeys(item, keysMaps))
+
+    return Object.entries(node).reduce((result, [key, value]) => {
+        const newKey = keysMaps[key] || key;
+        return {
+            ...result,
+            [newKey]: this.renameKeys(value, keysMaps)
+
+        };
+    }, {});
+  }
+
   ngOnInit() {
     console.log('careerPathData in carrepath=', this.careerPathData);
     /*
@@ -74,7 +88,20 @@ export class CareerPathComponent implements OnInit{
     }
     console.log('data=', data);
     */
-    this.dataSource = this.dataSourceBuilder.create(this.data);
+
+    /*
+   const newData = this.data.map(
+     item => {
+      return {
+        data: item.value,
+        children: item.children
+      };
+     }
+   );
+   */
+    const newData = this.renameKeys(this.data, {"value":"data"});
+    this.dataSource = this.dataSourceBuilder.create(newData);
+    console.log('this.dataSource =', this.dataSource );
   }
 
   updateSort(sortRequest: NbSortRequest): void {
@@ -91,44 +118,44 @@ export class CareerPathComponent implements OnInit{
 
   private data: TreeNode<FSEntry>[] = [
     {
-      data: { jobTitle: 'Projects', percent: 0.1 },
+      value: { jobTitle: 'Projects', percent: 0.1 },
       children: [
-        { data: { jobTitle: 'project-1.doc', percent: 0.2 } },
-        { data: { jobTitle: 'project-2.doc', percent: 0.2 } },
+        { value: { jobTitle: 'project-1.doc', percent: 0.2 } },
+        { value: { jobTitle: 'project-2.doc', percent: 0.2 } },
         {
-          data: { jobTitle: 'project-3', percent: 0.2 },
+          value: { jobTitle: 'project-3', percent: 0.2 },
           children: [
-            { data: { jobTitle: 'project-3A.doc', percent: 0.2 } },
-            { data: { jobTitle: 'project-3B.doc', percent: 0.2 } },
-            { data: { jobTitle: 'project-3C.doc', percent: 0.2 } },
+            { value: { jobTitle: 'project-3A.doc', percent: 0.2 } },
+            { value: { jobTitle: 'project-3B.doc', percent: 0.2 } },
+            { value: { jobTitle: 'project-3C.doc', percent: 0.2 } },
           ],
         },
-        { data: { jobTitle: 'project-4.docx', percent: 0.2 } },
+        { value: { jobTitle: 'project-4.docx', percent: 0.2 } },
       ],
     },
     {
-      data: { jobTitle: 'Reports', percent: 0.2 },
+      value: { jobTitle: 'Reports', percent: 0.2 },
       children: [
         {
-          data: { jobTitle: 'Report 1', percent: 0.2 },
+          value: { jobTitle: 'Report 1', percent: 0.2 },
           children: [
-            { data: { jobTitle: 'report-1.doc', percent: 0.2 } },
+            { value: { jobTitle: 'report-1.doc', percent: 0.2 } },
           ],
         },
         {
-          data: { jobTitle: 'Report 2', percent: 0.2 },
+          value: { jobTitle: 'Report 2', percent: 0.2 },
           children: [
-            { data: { jobTitle: 'report-2.doc', percent: 0.2 } },
-            { data: { jobTitle: 'report-2-note.txt', percent: 0.2 } },
+            { value: { jobTitle: 'report-2.doc', percent: 0.2 } },
+            { value: { jobTitle: 'report-2-note.txt', percent: 0.2 } },
           ],
         },
       ],
     },
     {
-      data: { jobTitle: 'Other', percent: 0.32 },
+      value: { jobTitle: 'Other', percent: 0.32 },
       children: [
-        { data: { jobTitle: 'backup.bkp', percent: 0.32 } },
-        { data: { jobTitle: 'secret-note.txt', percent: 0.32 } },
+        { value: { jobTitle: 'backup.bkp', percent: 0.32 } },
+        { value: { jobTitle: 'secret-note.txt', percent: 0.32 } },
       ],
     },
   ];
